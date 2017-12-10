fc354a6ce06c2b460f15224b2b03ea0afb4b5099
Jacob <jyggalagjones@gmail.com>
2017-12-09 22:37:21 -0500

    Added checks to make sure user cannot order if there is already 12 or more pizzas in the current order, imposed the price limit of 100 Dollars.

--- PizzaStore/PizzaStore.Library/Order.cs
+++ PizzaStore/PizzaStore.Library/Order.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace PizzaStore.Library
 {
@@ -23,8 +24,15 @@ namespace PizzaStore.Library
         }
         public void AddPizzatoOrder(Pizza pizzatoadd)
         {
-            pizzastoorder.Add(pizzatoadd);
-            TotalPrice += pizzatoadd.GetPrice();
+            if (pizzastoorder.Count < 12 || TotalPrice + pizzatoadd.GetPrice() > 100.00)
+            {
+                pizzastoorder.Add(pizzatoadd);
+                TotalPrice += pizzatoadd.GetPrice();
+            }
+            else
+            {
+                Debug.WriteLine("Error! Cannot order more than 12 pizzas, and price cannot exceed 100 US Dollars.");
+            }
         }
         public void RemovePizzaFromOrder(Pizza pizzatoremove)
         {
